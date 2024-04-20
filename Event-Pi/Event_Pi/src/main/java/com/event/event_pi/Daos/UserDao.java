380d3631bbdcf7026c0e92dbc53f1e2cdbab3158
MAALEJMED <medmohamed.maalej@gmail.com>
2024-04-20 22:52:30 +0100

    Integrate User entity from user-microservice

--- Event-Pi/Event_Pi/src/main/java/com/event/event_pi/Daos/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.event.event_pi.Daos;
-
-import com.event.event_pi.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User,Long> {
-}
