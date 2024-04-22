7e5973a7902b89598ac8baa5617c89dc57767f23
JalelDridi <med.jalel.dridi@gmail.com>
2024-04-22 13:37:00 +0100

    Configured Config server and service registry for user microservice

--- user-microservice/src/main/java/com/esprit/usermicroservice/servicesImpl/JwtAuthenticationFilter.java
+++ user-microservice/src/main/java/com/esprit/usermicroservice/servicesImpl/JwtAuthenticationFilter.java
@@ -1,11 +1,13 @@
 package com.esprit.usermicroservice.servicesImpl;
 
 import com.esprit.usermicroservice.services.JwtService;
+import jakarta.annotation.Resource;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,11 +22,14 @@ import java.io.IOException;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
