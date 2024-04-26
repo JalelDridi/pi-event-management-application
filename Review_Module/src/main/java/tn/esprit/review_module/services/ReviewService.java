32ce09fbce107041b6f1847861c472e1fd33f71b
IlefChammem <ilef.chammem@esprit.tn>
2024-04-26 18:12:04 +0100

    updated Review MS

--- Review_Module/src/main/java/tn/esprit/review_module/services/ReviewService.java
+++ Review_Module/src/main/java/tn/esprit/review_module/services/ReviewService.java
@@ -1,7 +1,6 @@
 package tn.esprit.review_module.services;
 
 import tn.esprit.review_module.entities.Review;
-
 import java.util.List;
 
 public interface ReviewService {
@@ -10,6 +9,10 @@ public interface ReviewService {
     public Review findreviewByreviewid(Long id);
     public List<Review> getAllReviews();
 
-     public Review UpdateReview(Review review,long id);
+    public Review UpdateReview(Review review,long id);
+
+    public List<Review> findReviewsbyEventID(Long eventid);
+
+    public  void deleteReview(Long id);
 
 }
