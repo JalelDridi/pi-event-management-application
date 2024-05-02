d053f56d25bbce681cb54278c5639120772ce978
amin-rm <ramdhaniahmedamine@gmail.com>
2024-05-02 16:17:58 +0100

    Create the skeleton of the final components

--- event-orchestrator-frontend/src/app/app.module.ts
+++ event-orchestrator-frontend/src/app/app.module.ts
@@ -19,6 +19,14 @@ import {ActivateAccountComponent} from "./pages/activate-account/activate-accoun
 import {HttpTokenInterceptor} from "./userservices/interceptor/http-token.interceptor";
 import {CodeInputModule} from "angular-code-input";
 import { TestComponent } from './test/test.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { AllEventsComponent } from './pages/all-events/all-events.component';
+import { MyParticipationsComponent } from './pages/my-participations/my-participations.component';
+import { MyEventsComponent } from './pages/my-events/my-events.component';
+import { CreateEventRequestComponent } from './pages/create-event-request/create-event-request.component';
+import { SubmitAFeedbackComponent } from './pages/submit-a-feedback/submit-a-feedback.component';
+import { RessourcesComponent } from './pages/ressources/ressources.component';
 
 
 
@@ -45,7 +53,15 @@ import { TestComponent } from './test/test.component';
     ResourceTypeComponent,
     AddResourceComponent,
     UpdateResourceComponent,
-    TestComponent
+    TestComponent,
+    HomeComponent,
+    AdminDashboardComponent,
+    AllEventsComponent,
+    MyParticipationsComponent,
+    MyEventsComponent,
+    CreateEventRequestComponent,
+    SubmitAFeedbackComponent,
+    RessourcesComponent
   ],
   providers: [
     ResourceService,
---
+++ event-orchestrator-frontend/src/app/pages/my-participations/my-participations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-participations',
+  templateUrl: './my-participations.component.html',
+  styleUrls: ['./my-participations.component.css']
+})
+export class MyParticipationsComponent {
+
+}
