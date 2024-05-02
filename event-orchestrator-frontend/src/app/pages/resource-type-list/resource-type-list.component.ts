bd9760639cfab39772824af4ff5108298a987c42
emna <emna.khlifaoui@esprit.tn>
2024-05-02 14:15:18 +0100

    updating front

---
+++ event-orchestrator-frontend/src/app/pages/resource-type-list/resource-type-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resource-type-list',
+  templateUrl: './resource-type-list.component.html',
+  styleUrls: ['./resource-type-list.component.css']
+})
+export class ResourceTypeListComponent {
+
+}
