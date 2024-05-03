fb3fedf1d1c44c098ad3bc9e3b41ebf6746b5043
amin-rm <ramdhaniahmedamine@gmail.com>
2024-05-03 20:46:48 +0100

    Refactor: secured logout process by deleting person info when logging out

--- event-orchestrator-frontend/src/app/components/navbar/navbar.component.ts
+++ event-orchestrator-frontend/src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { ROUTES } from '../sidebar/sidebar.component';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { Router } from '@angular/router';
+import {EventUserDto} from "../../userservices/models/event-user-dto";
+import {UserService} from "../../userservices/services/user.service";
 
 @Component({
   selector: 'app-navbar',
@@ -9,15 +11,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  user : EventUserDto;
   public focus;
   public listTitles: any[];
   public location: Location;
-  constructor(location: Location,  private element: ElementRef, private router: Router) {
+  constructor(location: Location,  private element: ElementRef, private router: Router, private userService: UserService) {
     this.location = location;
   }
 
+
   ngOnInit() {
+    // get user details of the logged in user
+    // Retrieve the user ID from local storage or a service after login
+    const userId = localStorage.getItem('userId'); // Assuming user ID is stored in local storage
+
+    if (userId) {
+      // If user ID is available, fetch user information
+      this.userService.getUserById({ userId }).subscribe(
+        (user: EventUserDto) => {
+          this.user = user; // Set the fetched user information
+        },
+        (error) => {
+          console.error('Error fetching user information:', error);
+        }
+      );
+    } else {
+      console.error('User ID not found');
+    }
     this.listTitles = ROUTES.filter(listTitle => listTitle);
+
   }
   getTitle(){
     var titlee = this.location.prepareExternalUrl(this.location.path());
@@ -31,6 +53,15 @@ export class NavbarComponent implements OnInit {
         }
     }
     return 'Dashboard';
+  }
+
+  logout(): void {
+    // Remove userId and token from cookies
+    localStorage.removeItem('userId');
+    localStorage.removeItem('token');
+
+    // Redirect to login page
+    this.router.navigate(['/login']);
   }
 
 }
