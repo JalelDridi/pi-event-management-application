52394adb2b80ea47cfd4d0710c3bbc20da0aeb65
emna <emna.khlifaoui@esprit.tn>
2024-05-04 22:10:06 +0100

    updating Resource entity

--- Resource-Module/src/main/java/com/esprit/pidev/resourcemodule/controllers/ResourceTypeController.java
+++ Resource-Module/src/main/java/com/esprit/pidev/resourcemodule/controllers/ResourceTypeController.java
@@ -1,12 +1,10 @@
 package com.esprit.pidev.resourcemodule.controllers;
 
-import com.esprit.pidev.resourcemodule.entities.Resource;
+
 import com.esprit.pidev.resourcemodule.entities.ResourceType;
 import com.esprit.pidev.resourcemodule.services.ResourceTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
