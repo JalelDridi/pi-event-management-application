5a18c1e68651ec8a6d0aab4bd044d5dc130937b1
amin-rm <ramdhaniahmedamine@gmail.com>
2024-05-18 11:11:29 +0100

    Ready for local deployment

--- event-orchestrator-frontend/src/app/pages/resource-type/resource-type.service.ts
+++ event-orchestrator-frontend/src/app/pages/resource-type/resource-type.service.ts
@@ -2,21 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ResourceType } from './resource-type';
 import { Observable } from 'rxjs';
+import {environment} from "../../../environments/environment.prod";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResourceTypeService {
 
-   
-  private baseUrl='http://localhost:8093/api/ResourceTypes';
+
+  private baseUrl= 'http://localhost:8093/api/ResourceTypes';
 
   constructor(private httpClient : HttpClient) { }
 
 
   getResourceTypes(): Observable<ResourceType[]> {
     return this.httpClient.get<ResourceType[]>(this.baseUrl + '/allResourceTypes');
-  
+
   }
 
 
@@ -32,7 +33,7 @@ export class ResourceTypeService {
     const url = `${this.baseUrl}/updateResourceType/${resourceTypeID}`;
     return this.httpClient.put<ResourceType>(url, resourceType);
   }
-  
+
   getResourceType(resourceTypeID: number): Observable<any> {
     return this.httpClient.get(this.baseUrl+'/'+resourceTypeID);
   }
--- event-orchestrator-frontend/src/environments/environment.prod.ts
+++ event-orchestrator-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+
+  apiUrl: 'http://apigateway:8060',
+
+  adminUrl: 'http://adminservice:8092',
+
+  resourceUrl: 'http://resourceservice:8090',
+
+  reviewUrl: 'http://reviewservice:8093',
 };
